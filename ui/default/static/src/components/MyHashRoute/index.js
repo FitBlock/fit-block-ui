e53c819eb8751dad324af24a2f91e8da590fccd5
zy445566 <zyct@vip.qq.com>
2020-02-14 15:21:09 +0800

    fix hash router

--- ui/default/static/src/components/MyHashRoute/index.js
+++ ui/default/static/src/components/MyHashRoute/index.js
@@ -4,6 +4,20 @@ export default class MyHashRoute extends HTMLContent {
         super();
         this.path = this.getAttribute('path');
         this.tag = this.getAttribute('tag');
+        this.renderView()
+        this.addListen();
+    }
+    addListen() {
+        const body = document.querySelector('body')
+        const beforeHashChange = body.onhashchange;
+        body.onhashchange = ()=>{
+            if(typeof beforeHashChange==='function'){
+                beforeHashChange()
+            }
+            this.renderView()
+        }
+    }
+    renderView() {
         const html = window.location.hash==this.path?`<${this.tag}/>`:'';
         this.render(html)
     }
--- ui/default/static/src/pages/MyHome/index.js
+++ ui/default/static/src/pages/MyHome/index.js
@@ -1,5 +1,6 @@
 import indexHtml from './index.html'
 import HTMLContent from '@/components/HTMLContent'
+import blockCore from 'fit-block-core/build/indexWeb';
 import enUS from "./locale/en-US";
 import zhCN from "./locale/zh-CN";
 import myI18n from "my-i18n";
@@ -12,6 +13,29 @@ export default class MyHome extends HTMLContent {
         super();
         const transData = this.getTrans()
         this.render(indexHtml,{...transData})
+        this.addRandBtnListen()
+        this.addIntoBtnListen()
+    }
+
+    addRandBtnListen() {
+        const randBtn = this.shadow.querySelector(".private-key-rand-btn")
+        randBtn.addEventListener('click',()=>{
+            const privateKeyByRand = blockCore.genPrivateKeyByRand()
+            const privateKeyInput = this.shadow.querySelector(".private-key-input")
+            privateKeyInput.value = privateKeyByRand
+        })
+    }
+
+    addIntoBtnListen() {
+        const intoBtn = this.shadow.querySelector(".into-btn")
+        intoBtn.addEventListener('click',()=>{
+            const privateKeyInput = this.shadow.querySelector(".private-key-input")
+            const privateKeyPattern = new RegExp('^[0-9a-f]{64}$','u');
+            if(!privateKeyPattern.test(privateKeyInput.value)){
+                return;
+            }
+            window.location.hash="#wallet"
+        })
     }
 
     getTrans() {
