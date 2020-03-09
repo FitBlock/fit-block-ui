81a3a66e3f4fa52d8c46b20f66428853ac566f22
zy445566 <zyct@vip.qq.com>
2020-03-09 13:34:17 +0800

    fix kkkk program

--- ui/default/index.ts
+++ ui/default/index.ts
@@ -6,6 +6,7 @@ import router from './router'
 import {join as pathJoin} from 'path'
 import fitBlockStore from 'fit-block-store'
 import postData from './middleware/postData'
+import { Worker, isMainThread } from  'worker_threads'
 // import fitBlockCore from 'fit-block-core'
 import fitBlockP2p from 'fit-block-p2p'
 export default class UIDefault extends UIBase {
@@ -13,7 +14,14 @@ export default class UIDefault extends UIBase {
         const storeServer = fitBlockStore.getServer();
         await storeServer.listen()
         // fitBlockCore.keepGodBlockData(await fitBlockCore.genGodBlock())
-        await fitBlockP2p.run()
+        if (isMainThread) {
+            const worker = new Worker(__filename);
+            worker.on('error', (err)=>{
+                // todo 写入文件，在dev模式再显示控制台
+            });
+        } else {
+            return await fitBlockP2p.run()
+        }
         const app = new Koa();
         app.use(koaStatic(pathJoin(__dirname, 'static'),{index:'index.html'}));
         app.use(postData)
