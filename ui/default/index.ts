306040ddc1d2b77695b4c8bf941dd72b5751ca26
zy445566 <zyct@vip.qq.com>
2020-03-13 09:45:15 +0800

    修复p2p启动卡住问题

--- ui/default/index.ts
+++ ui/default/index.ts
@@ -6,29 +6,21 @@ import router from './router'
 import {join as pathJoin} from 'path'
 import fitBlockStore from 'fit-block-store'
 import postData from './middleware/postData'
-import { Worker, isMainThread } from  'worker_threads'
 import fitBlockCore from 'fit-block-core'
-const logger = fitBlockCore.getLogger()
 import fitBlockP2p from 'fit-block-p2p'
 export default class UIDefault extends UIBase {
     async start():Promise<void> {
         const storeServer = fitBlockStore.getServer();
         await storeServer.listen()
         // fitBlockCore.keepGodBlockData(await fitBlockCore.genGodBlock())
-        if (isMainThread) {
-            const worker = new Worker(__filename);
-            worker.on('error', (err)=>{
-                logger.warn(err);
-            });
-        } else {
-            /**
-             * grpc只要在多线程使用会报C++模块引用失败Error:Module did not self-register.
-             * 导致p2p服务没有真正启动，现在先考虑修复方案
-             * 具体可参考：
-             * https://github.com/grpc/grpc-node/issues/778
-             */
-            return await fitBlockP2p.run()
-        }
+        /**
+         * fitBlockP2p.run不要加await，这里作为后台任务
+         * 原来这里使用了worker多线程，但是grpc只要在多线程使用会报C++模块引用失败Error:Module did not self-register.
+         * 导致p2p服务没有真正启动，现在先考虑修复方案
+         * 具体可参考：
+         * https://github.com/grpc/grpc-node/issues/778
+         */
+        fitBlockP2p.run()
         const app = new Koa();
         app.use(koaStatic(pathJoin(__dirname, 'static'),{index:'index.html'}));
         app.use(postData)
